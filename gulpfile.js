33471595786762e5b6056f07b8c7eefbdc293618
danlauby <dmlauby@gmail.com>
2017-03-17 09:38:54 -0700

    Create gulp task that concats files to tmp

--- gulpfile.js
+++ gulpfile.js
@@ -54,3 +54,9 @@ gulp.task('jsBrowserify', ['concatInterface'], function() {
     .pipe(source('app.js'))
     .pipe(gulp.dest('./build/js'));
 });
+
+gulp.task('concatInterface', function() {
+  return gulp.src(['./js/*-interface.js'])
+    .pipe(concat('allConcat.js'))
+    .pipe(gulp.dest('./tmp'));
+});
