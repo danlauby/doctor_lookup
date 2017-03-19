b8a3cec63adab47ab6cd83099231f12946bce864
danlauby <dmlauby@gmail.com>
2017-03-18 18:25:20 -0700

    Add img and bio of each matching doctor

--- js/doctor.js
+++ js/doctor.js
@@ -3,10 +3,10 @@ var apiKey = require('./../.env').apiKey;
 function Doctor(){}
 
 Doctor.prototype.getDoctorNames = function(medicalIssue) {
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey).then(function(response) {
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=2&user_key=' + apiKey).then(function(response) {
     console.log(response);
     for(var i = 0; i < response.data.length; i++) {
-      $('#doctor_list').append("<li>" + response.data[i].profile.first_name + " " + response.data[i].profile.last_name + "</li>");
+      $('#doctor_list').append("<h2>" + response.data[i].profile.first_name + " " + response.data[i].profile.last_name + "</h2><img src='" + response.data[i].profile.image_url +"'><p>" + response.data[i].profile.bio + "</p>");
     }
   }).fail(function(error) {
     $('#doctor_list').text(error.responseJSON.message);
