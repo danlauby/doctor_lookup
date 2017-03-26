aa75033479ceaf1f2ffafe5886ecc580959dfea1
danlauby <dmlauby@gmail.com>
2017-03-26 12:47:24 -0700

    Create doctorProfile callback that is called as displayDoctor to seperate business and user concerns

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,11 +1,15 @@
 var Doctor = require('./../js/doctor.js').doctorModule;
 
+var displayDoctor = function(firstName, lastName, doctorImg, doctorBio) {
+  $('#doctor_list').append("<div class='well'><h2>" + firstName + " " + lastName + "</h2><img src='" + doctorImg +"' alt='image of Dr "+ lastName +"'><p>" + doctorBio + "</p></div>");
+};
+
 $(document).ready(function() {
   var currentDoctor = new Doctor();
   $('#find_doctor').click(function() {
     var medical_issue = $('#medical_issue').val();
     $('#medical_issue').val("");
-    currentDoctor.getDoctor(medical_issue);
+    currentDoctor.getDoctor(medical_issue, displayDoctor);
     $('doctor_list').text("");
   });
 });
--- js/doctor.js
+++ js/doctor.js
@@ -2,11 +2,14 @@ var apiKey = require('./../.env').apiKey;
 
 function Doctor(){}
 
-Doctor.prototype.getDoctor = function(medicalIssue) {
+Doctor.prototype.getDoctor = function(medicalIssue, doctorProfile) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey).then(function(response) {
-    console.log(response);
     for(var i = 0; i < response.data.length; i++) {
-      $('#doctor_list').append("<div class='well'><h2>" + response.data[i].profile.first_name + " " + response.data[i].profile.last_name + "</h2><img src='" + response.data[i].profile.image_url +"' alt='image of Dr "+ response.data[i].profile.last_name +"'><p>" + response.data[i].profile.bio + "</p></div>");
+      var firstName = response.data[i].profile.first_name;
+      var lastName = response.data[i].profile.last_name;
+      var doctorImg = response.data[i].profile.image_url;
+      var doctorBio = response.data[i].profile.bio;
+      doctorProfile(firstName, lastName, doctorImg, doctorBio);
     }
   }).fail(function(error) {
     $('#doctor_list').text(error.responseJSON.message);
